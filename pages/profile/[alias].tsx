8bb7977be79541a1d8be9a6a328ab1d029735794
Felix Adam <felix.adam@tx.group>
2023-02-13 22:04:03 +0100

    feat: initial profile and user pages.

--- pages/profile/[alias].tsx
+++ pages/profile/[alias].tsx
@@ -1,4 +1,8 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { Header } from '../../components/Header';
+
+import { Image, ImageOverlay, UserProfile, Headline } from '@smartive-education/pizza-hawaii';
+import User from '../../data/user.json';
 
 type Props = {
 	profile: {
@@ -7,10 +11,36 @@ type Props = {
 };
 
 export default function ProfilePage({ profile }: Props): InferGetServerSidePropsType<typeof getServerSideProps> {
+	const user = { ...User };
+	const changeBackgroundImage = () => {
+		console.log('changeBackgroundImage Logic here');
+	};
+
 	return (
-		<>
-			<h1>{profile.alias}</h1>
-		</>
+		<div className="bg-slate-100 relative mb-6">
+			<Header user={user} />
+			<h1>Edit your own Profile Page of: {profile.alias}</h1>
+			{/* eslint-disable-next-line @typescript-eslint/no-empty-function */}
+			<ImageOverlay
+				preset="edit"
+				buttonLabel={'change Background banner image'}
+				onClick={() => {
+					changeBackgroundImage;
+				}}
+			>
+				<Image src="//picsum.photos/seed/johndoe1/1600/1157/" alt="static alt image" preset="header" />
+			</ImageOverlay>
+			<div className="absolute right-8 bottom-0 translate-y-1/2 z-10">
+				<UserProfile
+					userName={user.userName}
+					avatar={user.avatarUrl}
+					size="XL"
+					border={true}
+					href="/"
+					buttonLabel="Change Avatar"
+				/>
+			</div>
+		</div>
 	);
 }
 
--- pages/user/[id].tsx
+++ pages/user/[id].tsx
@@ -1,18 +1,14 @@
 import { GetServerSideProps, InferGetServerSidePropsType } from 'next';
+import { Header } from '../../components/Header';
 
+import { Image, ImageOverlay, UserProfile, Headline } from '@smartive-education/pizza-hawaii';
+import User from '../../data/user.json';
+console.log('user', User);
 type Props = {
 	mumble: {
 		id: string;
 	};
 };
-
-export default function MumblePage({ mumble }: Props): InferGetServerSidePropsType<typeof getServerSideProps> {
-	return (
-		<>
-			<h1>{mumble.id}</h1>
-		</>
-	);
-}
 
 export const getServerSideProps: GetServerSideProps = async ({ query: { id } }) => {
 	return {
@@ -21,3 +17,37 @@ export const getServerSideProps: GetServerSideProps = async ({ query: { id } }) 
 		},
 	};
 };
+
+export default function UserPage({ mumble }: Props): InferGetServerSidePropsType<typeof getServerSideProps> {
+	const user = { ...User };
+	const changeBackgroundImage = () => {
+		console.log('changeBackgroundImage Logic here');
+	};
+
+	return (
+		<div className="bg-slate-100 relative mb-6">
+			<Header user={user} />
+			<h1>User Page of Mumble Dude {mumble.id}</h1>
+			<ImageOverlay
+				preset="edit"
+				buttonLabel={'change Background banner image'}
+				onClick={() => {
+					changeBackgroundImage;
+				}}
+			>
+				<Image src="//picsum.photos/seed/1400/1428/" alt="static alt image" preset="header" />
+			</ImageOverlay>
+			<div className="absolute right-8 bottom-0 translate-y-1/2 z-10">
+				<UserProfile
+					userName={user.userName}
+					avatar={user.avatarUrl}
+					size="XL"
+					border={true}
+					href="/"
+					buttonLabel="Change Avatar"
+				/>
+			</div>
+			<Headline level={3}>{user.userName}</Headline>
+		</div>
+	);
+}
